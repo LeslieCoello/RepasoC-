48301961c092492285054b76ef1186a4140ffa5a
Leslie <lesliecoello2005@gmail.com>
2026-03-12 13:56:49 -0500

    Ejercicio 3

---
+++ ejercicio3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std; 
+
+class Estudiante {
+public:
+	String nombre;
+	int edad;
+	
+	void mostrarDatos(){
+		cout << "Nombre: " << nombre << endl;
+		cout << "Edad: " << edad << endl;
+	}
+};
+
+int main (){
+	Estudiante e1;
+	e1.nombre = "Ana";
+	e1.edad = 21;
+	e1.mostrarDatos() ;
+	
+	return 0;
+}
+
